a70ce60b9e9f9af9d192a2417ed9ade397dec9e3
Your Name <you@example.com>
2021-02-22 16:08:21 -0500

    Removed mat files from git. Updated build script in preparation for Jenkins Upgated Git Ignore

--- Example2/discrete_tf_build.m
+++ Example2/discrete_tf_build.m
@@ -1,13 +1,16 @@
 %% Model Builder
-
 % Close all models.
 bdclose('all');
 % Clear matlab environment.
 clear;
+% Change to the build script folder
+cd(fileparts(mfilename('fullpath')));
+
+% Model To Build
 model = 'discrete_tf';
 % Run the model init script.
 feval([model '_init']);
 % Open the model.
 open_system(model);
-% 
+% RealTimeWorkshop Build 
 rtwbuild(model);
